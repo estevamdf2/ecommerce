18c000ebfac491715e6f0988c12d0154a5f05869
Marcos Sousa <>
2023-10-06 19:18:03 -0300

    Aula 3. Extraindo a camada do consumidor.

--- src/main/java/br/com/alura/ecommerce/NewOrder.java
+++ src/main/java/br/com/alura/ecommerce/NewOrder.java
@@ -20,7 +20,7 @@ public class NewOrder {
 
 
         var producer = new KafkaProducer<String, String>(properties());
-        for (int i=0; i<100; i++) {
+        for (int i=0; i<2; i++) {
             var key = UUID.randomUUID().toString();
             var value = key + ", 1001, 12000";
             var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
