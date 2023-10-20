ccc6ce198b44e322ff362f237f070539596cf9c4
Marcos Sousa <>
2023-10-20 18:53:40 -0300

    Realizando o envio de forma assincrona

--- service-users/src/main/java/br/com/alura/br/com/alura/ecommerce/BatchSendMessageService.java
+++ service-users/src/main/java/br/com/alura/br/com/alura/ecommerce/BatchSendMessageService.java
@@ -50,10 +50,11 @@ public class BatchSendMessageService {
         System.out.println("Topic: " +message.getPayload());
 
         for (User user : getAllUsers()) {
-            userDispatcher.send(message.getPayload(),
+            userDispatcher.sendAsync(message.getPayload(),
                     user.getUuid(),
                     message.getId().continueWith(BatchSendMessageService.class.getSimpleName()),
                     user);
+            System.out.println("Enviei para o user "+user);
         }
 
 
